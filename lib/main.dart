d47180e809a6e3200e61275a4008647fd30fe8ab
yang-jiu <108648913+yang-jiu@users.noreply.github.com>
2023-02-22 15:20:40 +0900

    [FEAT] 끼니 기록 리스트 구현

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 //import 'dart:html';
 
 import 'package:flutter/material.dart';
-import 'start.dart';
+//import 'start.dart';
+import 'record.dart';
 
 void main() {
   runApp(MyApp());
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: StartPage(),
+      home: Record(),
     );
   }
 }
