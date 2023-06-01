ba23239092d4ea0dad94add6284eb7647bd007a7
yang-jiu <108648913+yang-jiu@users.noreply.github.com>
2023-06-01 20:20:13 +0900

    [FEAT] 토스-스마트컨트랙트 페이지 연결

--- lib/smart_contract.dart
+++ lib/smart_contract.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart' show rootBundle;
 import 'package:http/http.dart';
+import 'package:temp_project/const/colors.dart';
+import 'package:temp_project/donate_pages/donate.dart';
+import 'package:temp_project/main.dart';
 import 'package:temp_project/sc_controller.dart';
 import 'package:web3dart/web3dart.dart';
+import 'const/colors.dart';
 
-void main() {
+/*void main() {
   runApp(const MyApp());
 }
 
@@ -15,23 +19,20 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Counter',
-      theme: ThemeData(
-        primarySwatch: Colors.green,
-      ),
-      home: const SmartContranct(title: 'Counter'),
+      home: const SmartContract(title: 'Counter'),
     );
   }
 }
-
-class SmartContranct extends StatefulWidget {
-  const SmartContranct({Key? key, required this.title}) : super(key: key);
+*/
+class SmartContract extends StatefulWidget {
+  const SmartContract({Key? key, required this.title}) : super(key: key);
   final String title;
 
   @override
-  State<SmartContranct> createState() => _SmartContranct();
+  State<SmartContract> createState() => _SmartContract();
 }
 
-class _SmartContranct extends State<SmartContranct> {
+class _SmartContract extends State<SmartContract> {
   //연결하려는 네트워크
   static const apiUrl =
       "https://sepolia.infura.io/v3/f49cf96589a248ddb26c8cdcc0a4e7a5";
@@ -49,7 +50,6 @@ class _SmartContranct extends State<SmartContranct> {
 
   // 스마트 컨트랙트 내의 함수 정의
   ContractFunction? _incrementFunction;
-  ContractFunction? _decrementFunction;
   ContractFunction? _checkFunction;
 
   BigInt _check = BigInt.from(0);
@@ -87,18 +87,6 @@ class _SmartContranct extends State<SmartContranct> {
     });
   }
 
-  Future<void> _decrementCounter() async {
-    await _client.sendTransaction(
-      _credentials,
-      Transaction.callContract(
-        contract: _contract!,
-        function: _decrementFunction!,
-        parameters: [BigInt.from(1)],
-      ),
-      chainId: 3,
-    );
-  }
-
   Future<void> _incrementCounter() async {
     await _client.sendTransaction(
       _credentials,
@@ -114,9 +102,7 @@ class _SmartContranct extends State<SmartContranct> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text(widget.title),
-        ),
+        backgroundColor: Colors.white,
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
@@ -124,30 +110,149 @@ class _SmartContranct extends State<SmartContranct> {
               Column(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: <Widget>[
+                  Container(
+                    padding: EdgeInsets.only(right: 12.0),
+                    child: Image.asset(
+                      'assets/ether.png',
+                    ),
+                  ),
+                  SizedBox(height: 15),
                   Text('0.002 ETH가', style: TextStyle(fontSize: 20.0)),
                   SizedBox(height: 15.0),
-
                   Text('기부단체에 전달됩니다', style: TextStyle(fontSize: 20.0)),
-                  // 값을 추가하는 함수
-                  // IconButton(
-                  //   icon: const Icon(Icons.add),
-                  //   onPressed: _incrementCounter,
-                  // ),
-                  Text('$_check'),
                 ],
               ),
               // 현재 값을 확인하는 버튼
               SizedBox(height: 20),
               ElevatedButton(
-                child: Text('기부금 전달', style: TextStyle(fontSize: 17.0)),
+                style: ElevatedButton.styleFrom(
+                  primary: PRIMARY_COLOR,
+                ),
+                child: Text('기부금 전달',
+                    style: TextStyle(fontSize: 17.0, color: BLACK_COLOR)),
                 onPressed: () {
-                  //Future<double?> ether = EtherProvider().getEther(15000);
-                  _incrementCounter();
-                  _getCheck();
+                  Navigator.pushAndRemoveUntil(
+                      context,
+                      MaterialPageRoute(builder: (context) => SecondPage()),
+                      (route) => false);
+                  // Navigator.push(
+                  //   context,
+                  //   MaterialPageRoute(
+                  //     builder: (context) => SecondPage(),
+                  //   ),
+                  // );
+                  //_incrementCounter();
+                  //_getCheck();
                 },
               )
             ],
           ),
         ));
+  }
+}
+
+class SecondPage extends StatefulWidget {
+  const SecondPage({Key? key}) : super(key: key);
+  @override
+  State<SecondPage> createState() => _SecondPageState();
+}
+
+class _SecondPageState extends State<SecondPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.white,
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Image.asset(
+                    'assets/ether.png',
+                  ),
+                  SizedBox(height: 15),
+                  Text('0.002 ETH', style: TextStyle(fontSize: 20.0)),
+                  SizedBox(height: 15.0),
+
+                  Text('기부단체에 전달 완료', style: TextStyle(fontSize: 20.0)),
+                  // 값을 추가하는 함수
+                  // IconButton(
+                  //   icon: const Icon(Icons.add),
+                  //   onPressed: _incrementCounter,
+                  // ),
+                ],
+              ),
+              // 현재 값을 확인하는 버튼
+              SizedBox(height: 20),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  primary: PRIMARY_COLOR,
+                ),
+                child: Text(
+                  '확인',
+                  style: TextStyle(fontSize: 17.0, color: BLACK_COLOR),
+                ),
+                onPressed: () {
+                  Navigator.pushAndRemoveUntil(
+                      context,
+                      MaterialPageRoute(builder: (context) => HomePage()),
+                      (route) => false);
+                },
+              )
+            ],
+          ),
+        ));
+  }
+}
+
+class ThirdPage extends StatefulWidget {
+  const ThirdPage({Key? key}) : super(key: key);
+  @override
+  State<ThirdPage> createState() => _ThirdPageState();
+}
+
+class _ThirdPageState extends State<ThirdPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Image.asset('assets/Ether.jpeg',
+                  fit: BoxFit.contain // 이미지가 자리를 차지하도록 설정
+                  ),
+              SizedBox(height: 15),
+              Text('0.002 ETH', style: TextStyle(fontSize: 20.0)),
+              SizedBox(height: 15.0),
+
+              Text('기부단체에 전달 완료', style: TextStyle(fontSize: 20.0)),
+              // 값을 추가하는 함수
+              // IconButton(
+              //   icon: const Icon(Icons.add),
+              //   onPressed: _incrementCounter,
+              // ),
+            ],
+          ),
+          // 현재 값을 확인하는 버튼
+          SizedBox(height: 20),
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              primary: PRIMARY_COLOR,
+            ),
+            child: Text(
+              '확인',
+              style: TextStyle(fontSize: 17.0, color: BLACK_COLOR),
+            ),
+            onPressed: () {},
+          )
+        ],
+      ),
+    ));
   }
 }
